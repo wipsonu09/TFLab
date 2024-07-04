7d757b44605563b6f5fbfa660e6d2d8d6a3df158
satish <myemail@gmail.com>
2024-07-05 01:00:53 +0530

    modified file

--- iam.tf
+++ iam.tf
@@ -1,6 +1,7 @@
 resource "aws_iam_user" "iam" {
 
-  name = upper(var.iam)
+  #name = upper(var.iam)
+  name = "TFsatish"
   tags = {
     name = "iamuser"
   }
--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3buck" {
-  bucket = var.s3buck
+  bucket = "satishtcs985421"
   
   tags = {
     name = "s3bucket"
